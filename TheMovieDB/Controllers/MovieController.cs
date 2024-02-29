ea1dd965aed1d5cb67d110ca974b77322b04a019
DiegoAParra <diego.1701812199@ucaldas.edu.co>
2024-02-28 21:45:11 -0500

    Generar factura

--- TheMovieDB/Controllers/MovieController.cs
+++ TheMovieDB/Controllers/MovieController.cs
@@ -1,5 +1,9 @@
-﻿using System.Threading.Tasks;
+﻿using Microsoft.Reporting.WebForms;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Web.Mvc;
+using TheMovieDB.Models;
 using TheMovieDB.Services;
 
 namespace TheMovieDB.Controllers
@@ -41,6 +45,92 @@ namespace TheMovieDB.Controllers
             {
                 return HttpNotFound();
             }
-        }        
+        }
+
+        [HttpPost]
+        public ActionResult AddToCart(int movieId, string title)
+        {
+            var shoppingCartItem = new ShoppingCartItem
+            {
+                MovieId = movieId,
+                Title = title,
+                Price = 1000
+            };
+
+            List<ShoppingCartItem> shoppingCart = Session["Cart"] as List<ShoppingCartItem> ?? new List<ShoppingCartItem>();
+            shoppingCart.Add(shoppingCartItem);
+            Session["Cart"] = shoppingCart;
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Cart()
+        {
+            List<ShoppingCartItem> shoppingCart = Session["Cart"] as List<ShoppingCartItem> ?? new List<ShoppingCartItem>();
+            return View(shoppingCart);
+        }
+
+        public ActionResult RemoveFromCart(int movieId)
+        {
+            var shoppingCart = Session["ShoppingCart"] as List<ShoppingCartItem>;
+
+            if (shoppingCart != null)
+            {
+                var itemToRemove = shoppingCart.FirstOrDefault(item => item.MovieId == movieId);
+
+                if (itemToRemove != null)
+                {
+                    shoppingCart.Remove(itemToRemove);
+                    Session["ShoppingCart"] = shoppingCart;
+                }
+            }
+
+            return RedirectToAction("Cart");
+        }
+
+        public ActionResult Pay(string format = "PDF")
+        {
+            var list = Session["Cart"] as List<ShoppingCartItem>;
+            List<ShoppingCartItem> recordsList = new List<ShoppingCartItem>();
+
+            foreach (var cartItem in list)
+            {
+                recordsList.Add(
+                    new ShoppingCartItem()
+                    {
+                        MovieId = cartItem.MovieId,
+                        Title = cartItem.Title,
+                        Price = cartItem.Price,
+                    });
+            }
+
+            string reportPath = Server.MapPath("~/Reports/RdlcFiles/CartReport.rdlc");
+
+            LocalReport lr = new LocalReport();
+
+            lr.ReportPath = reportPath;
+            lr.EnableHyperlinks = true;
+
+            Warning[] warnings;
+            string[] streams;
+            byte[] renderedBytes;
+            string mimeType, encoding, fileNameExtension;
+
+            ReportDataSource datasource = new ReportDataSource("CartDataSet", recordsList);
+            lr.DataSources.Add(datasource);
+
+
+            renderedBytes = lr.Render(
+            format,
+            string.Empty,
+            out mimeType,
+            out encoding,
+            out fileNameExtension,
+            out streams,
+            out warnings
+            );
+
+            return File(renderedBytes, mimeType);
+        }
     }
 }
---
+++ TheMovieDB/Models/ShoppingCartItem.cs
@@ -0,0 +1,9 @@
+﻿namespace TheMovieDB.Models
+{
+    public class ShoppingCartItem
+    {
+        public int MovieId { get; set; }
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+    }
+}
